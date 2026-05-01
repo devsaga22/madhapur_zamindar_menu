956b31e103e8d88b151c9db21b5e2b655ec2d6f8
devsaga22 <sagarchokkara@gmail.com>
2026-05-01 11:19:57 +0530

    combo-menu ordered, pdf modified

--- src/components/ZamExport.js
+++ src/components/ZamExport.js
@@ -3,14 +3,14 @@ export const restaurantConfig = {
     tagline: "The Royal Taste of India",
     // Link to the main static menu image (place the image in public/assets/)
     menuImageUrls: [
-                    'zamindar_m-pg-1.jpg',
+                    'zamindar_m-pg-1.png',
                     'zamindar_m-pg-2.jpg',
-                    'zamindar_m-pg-3.png'
+                    'zamindar_m-pg-3.jpg'
                     
     ] ,
     phone: '+91-9642888994',
     // Link to the downloadable PDF (place the PDF in public/assets/)
-    menuPdfUrl: 'Zamindar-Menu.pdf',
+    menuPdfUrl: 'zamindar_menu.pdf',
     google_reviewURL:'https://shorturl.at/8NgOB',
     address:{
         metro:'opposite Pillar no -1731',
